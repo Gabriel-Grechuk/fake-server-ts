d2bc1f464bfd9b9ec5c8c0be8b4ac316d73a5ead
Gabriel-Grechuk <gabrielgrechuk@outlook.com>
2023-04-17 18:45:50 -0300

    #1 - Update config to load command line arguments

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,28 @@
 import "https://deno.land/std@0.183.0/dotenv/load.ts";
+
+import { parse } from "https://deno.land/std/flags/mod.ts";
+
+import * as log from "./log.ts";
 import { normalizePathString } from "./utils.ts";
 
+const commandLineArgs = parse(Deno.args);
+
+function validateArgs() {
+  const validArgs = ["port", "response-files-path"];
+  for (const arg of Object.keys(commandLineArgs)) {
+    if (arg !== "_" && !validArgs.includes(arg)) {
+      log.error("Invalid command line argument:", arg);
+      Deno.exit(1);
+    }
+  }
+}
+validateArgs();
+
 export const config = {
-  port: parseInt(Deno.env.get("PORT")) || 3000,
-  responseFilesPath: normalizePathString(Deno.env.get("RESPONSE_FILES_PATH")) || "responses",
+  port:
+    parseInt(commandLineArgs["port"]) || parseInt(Deno.env.get("PORT")) || 3000,
+  responseFilesPath:
+    normalizePathString(commandLineArgs["response-files-path"]) ||
+    normalizePathString(Deno.env.get("RESPONSE_FILES_PATH")) ||
+    "responses",
 };
