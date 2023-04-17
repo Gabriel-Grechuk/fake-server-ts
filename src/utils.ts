d54a24765962ca23a8346f0b04d2b8b2d3a00e3b
Gabriel-Grechuk <gabrielgrechuk@outlook.com>
2023-04-17 17:44:54 -0300

    #1 - utils.ts

---
+++ src/utils.ts
@@ -0,0 +1,32 @@
+export function normalizePathString(path: string): string {
+  if (!path) return undefined;
+  let normalized = path.trim();
+
+  if (normalized.charAt(normalized.length - 1) === "/")
+    normalized = normalized.slice(0, -1);
+
+  return normalized;
+}
+
+export function validateFields(fields: string[], obj: unknown): boolean {
+  const keys = Object.keys(obj);
+  for (const field of fields) {
+    if (!keys.includes(field)) return false;
+  }
+  return true;
+}
+
+export function deleteUnusedFields(
+  fieldsToKeep: string[],
+  obj: unknown
+): unknown {
+  const response = {};
+
+  fieldsToKeep.forEach((field) => {
+    Object.assign(response, {
+      [field]: obj[field],
+    });
+  });
+
+  return response;
+}
