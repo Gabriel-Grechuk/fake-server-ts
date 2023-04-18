122d432c47688c7efbc745dd539616defe27bd8c
Gabriel-Grechuk <gabrielgrechuk@outlook.com>
2023-04-18 15:58:09 -0300

    #1 - Allow net running deno

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 command -v deno >/dev/null 2>&1 || { printf "[ \033[31mERROR\033[0m ] You need \033[96mdeno\033[0m to run this project.\n"; exit 1; }
 
-deno run --allow-env --allow-read src/main.ts $*
+deno run --allow-env --allow-read --allow-net src/main.ts $*
