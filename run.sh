4613c50d15c5e3655df7af1cbd6a23cd4a265e0f
Gabriel-Grechuk <gabrielgrechuk@outlook.com>
2023-04-19 11:04:47 -0300

    #1 - Run deno in UTC

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 
 command -v deno >/dev/null 2>&1 || { printf "[ \033[31mERROR\033[0m ] You need \033[96mdeno\033[0m to run this project.\n"; exit 1; }
 
+TZ=UTC
 deno run --allow-env --allow-read --allow-net src/main.ts $*
+
